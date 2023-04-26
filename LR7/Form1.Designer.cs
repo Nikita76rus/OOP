319b12bd269eff1128cec5fbc5c51755e641e633
Nikita76rus <56645414+Nikita76rus@users.noreply.github.com>
2023-04-26 15:29:09 +0300

    Add files via upload

---
+++ LR7/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace LR7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.encode = new System.Windows.Forms.Button();
+            this.decode = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.load = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.input.Location = new System.Drawing.Point(12, 12);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(491, 26);
+            this.input.TabIndex = 0;
+            // 
+            // output
+            // 
+            this.output.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.output.Location = new System.Drawing.Point(12, 233);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(491, 26);
+            this.output.TabIndex = 1;
+            // 
+            // encode
+            // 
+            this.encode.BackColor = System.Drawing.Color.Gold;
+            this.encode.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.encode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.encode.Location = new System.Drawing.Point(12, 57);
+            this.encode.Name = "encode";
+            this.encode.Size = new System.Drawing.Size(153, 46);
+            this.encode.TabIndex = 2;
+            this.encode.Text = "ЗАКОДИРОВАТЬ";
+            this.encode.UseVisualStyleBackColor = false;
+            this.encode.Click += new System.EventHandler(this.encode_Click);
+            // 
+            // decode
+            // 
+            this.decode.BackColor = System.Drawing.Color.Gold;
+            this.decode.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.decode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.decode.Location = new System.Drawing.Point(350, 57);
+            this.decode.Name = "decode";
+            this.decode.Size = new System.Drawing.Size(153, 46);
+            this.decode.TabIndex = 3;
+            this.decode.Text = "РАСПОЗНАТЬ";
+            this.decode.UseVisualStyleBackColor = false;
+            this.decode.Click += new System.EventHandler(this.decode_Click);
+            // 
+            // save
+            // 
+            this.save.BackColor = System.Drawing.Color.SpringGreen;
+            this.save.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.save.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.save.Location = new System.Drawing.Point(12, 181);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(153, 46);
+            this.save.TabIndex = 4;
+            this.save.Text = "СОХРАНИТЬ";
+            this.save.UseVisualStyleBackColor = false;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // load
+            // 
+            this.load.BackColor = System.Drawing.Color.SpringGreen;
+            this.load.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.load.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.load.Location = new System.Drawing.Point(350, 181);
+            this.load.Name = "load";
+            this.load.Size = new System.Drawing.Size(153, 46);
+            this.load.TabIndex = 5;
+            this.load.Text = "ЗАГРУЗИТЬ";
+            this.load.UseVisualStyleBackColor = false;
+            this.load.Click += new System.EventHandler(this.load_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(171, 57);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(173, 170);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "PNG|*.png|JPEG|*.jpg|GIF|*.gif|BMP|*.bmp.";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "PNG|*.png|JPEG|*.jpg|GIF|*.gif|BMP|*.bmp.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 273);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.load);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.decode);
+            this.Controls.Add(this.encode);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.input);
+            this.Name = "Form1";
+            this.Text = "LR7";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Button encode;
+        private System.Windows.Forms.Button decode;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button load;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ LR7/Form1.cs
@@ -0,0 +1,56 @@
+﻿using MessagingToolkit.QRCode.Codec;
+using MessagingToolkit.QRCode.Codec.Data;
+using System;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LR7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void load_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.ImageLocation = openFileDialog1.FileName;
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                    pictureBox1.Image.Save(saveFileDialog1.FileName);
+        }
+
+        private void encode_Click(object sender, EventArgs e)
+        {
+            string txt = input.Text;
+            QRCodeEncoder coder = new QRCodeEncoder();
+            //Bitmap qrcode = coder.Encode(textBox1.Text);
+            Bitmap qrcode = coder.Encode(txt, Encoding.UTF8);
+            pictureBox1.Image = qrcode as Image;
+        }
+
+        private void decode_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Bitmap decImg = new Bitmap(pictureBox1.Image);
+                QRCodeBitmapImage qrImg = new QRCodeBitmapImage(decImg);
+                QRCodeDecoder decoder = new QRCodeDecoder();
+                //decoder.decode(qrImg);
+                output.Text = decoder.Decode(qrImg, Encoding.UTF8);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(
+                "нет распознаваемого образа, ошибка - \n\r" + err);
+            }
+        }
+    }   
+}
