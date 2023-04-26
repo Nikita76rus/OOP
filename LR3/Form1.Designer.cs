daf37b0f9f3641e8c7b35f4ac41e1591d0f6ab96
Nikita76rus <56645414+Nikita76rus@users.noreply.github.com>
2023-04-26 14:28:20 +0300

    Add files via upload

---
+++ LR3/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace LR3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.inpX0 = new System.Windows.Forms.TextBox();
+            this.inpKx = new System.Windows.Forms.TextBox();
+            this.inpDx = new System.Windows.Forms.TextBox();
+            this.inpA = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.start = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X0 =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kx =";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(10, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Dx =";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "A =";
+            // 
+            // inpX0
+            // 
+            this.inpX0.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inpX0.Location = new System.Drawing.Point(51, 9);
+            this.inpX0.Name = "inpX0";
+            this.inpX0.Size = new System.Drawing.Size(144, 26);
+            this.inpX0.TabIndex = 4;
+            // 
+            // inpKx
+            // 
+            this.inpKx.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inpKx.Location = new System.Drawing.Point(51, 41);
+            this.inpKx.Name = "inpKx";
+            this.inpKx.Size = new System.Drawing.Size(144, 26);
+            this.inpKx.TabIndex = 5;
+            // 
+            // inpDx
+            // 
+            this.inpDx.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inpDx.Location = new System.Drawing.Point(51, 73);
+            this.inpDx.Name = "inpDx";
+            this.inpDx.Size = new System.Drawing.Size(144, 26);
+            this.inpDx.TabIndex = 6;
+            // 
+            // inpA
+            // 
+            this.inpA.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.inpA.Location = new System.Drawing.Point(51, 105);
+            this.inpA.Name = "inpA";
+            this.inpA.ReadOnly = true;
+            this.inpA.Size = new System.Drawing.Size(144, 26);
+            this.inpA.TabIndex = 7;
+            this.inpA.Text = "0,75";
+            this.inpA.UseWaitCursor = true;
+            // 
+            // output
+            // 
+            this.output.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.output.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.output.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.output.Location = new System.Drawing.Point(12, 137);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.output.Size = new System.Drawing.Size(384, 232);
+            this.output.TabIndex = 8;
+            // 
+            // start
+            // 
+            this.start.BackColor = System.Drawing.Color.IndianRed;
+            this.start.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.start.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.start.Location = new System.Drawing.Point(12, 375);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(183, 35);
+            this.start.TabIndex = 9;
+            this.start.Text = "Вычислить";
+            this.start.UseVisualStyleBackColor = false;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // clear
+            // 
+            this.clear.BackColor = System.Drawing.Color.RosyBrown;
+            this.clear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.clear.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clear.Location = new System.Drawing.Point(201, 375);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(195, 35);
+            this.clear.TabIndex = 10;
+            this.clear.Text = "Очистить";
+            this.clear.UseVisualStyleBackColor = false;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 418);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.inpA);
+            this.Controls.Add(this.inpDx);
+            this.Controls.Add(this.inpKx);
+            this.Controls.Add(this.inpX0);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "LR3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox inpX0;
+        private System.Windows.Forms.TextBox inpKx;
+        private System.Windows.Forms.TextBox inpDx;
+        private System.Windows.Forms.TextBox inpA;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button clear;
+    }
+}
+
---
+++ LR3/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace LR3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            // Считывание введенных данных
+            double x0 = Convert.ToDouble(inpX0.Text);
+            double xk = Convert.ToDouble(inpKx.Text);
+            double dx = Convert.ToDouble(inpDx.Text);
+            double a = Convert.ToDouble(inpA.Text);
+            output.Text = "Работу выполнил ст. Захаров Н.А. 22-ИБ429" + Environment.NewLine; //вывод первой строки
+
+            // Цикл для подсчета
+            double x = x0;
+            while (x <= (xk + dx / 2))
+            {
+                double y = a * Math.Log(x);
+                output.Text += "x=" + Convert.ToString(x) +
+                "; y=" + Convert.ToString(y) + Environment.NewLine;
+                x = x + dx;
+            }
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            //Очистка введенных и полученых данных
+            inpX0.Text = null;
+            inpKx.Text = null;
+            inpDx.Text = null;
+            output.Text = null;
+        }
+    }
+}
