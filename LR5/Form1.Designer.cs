fbbcf656b83048e6e62684709bb458c06765fc73
Nikita76rus <56645414+Nikita76rus@users.noreply.github.com>
2023-04-26 14:54:47 +0300

    Add files via upload

---
+++ LR5/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace LR5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fill = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.TextBox();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fill
+            // 
+            this.fill.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.fill.Location = new System.Drawing.Point(12, 371);
+            this.fill.Name = "fill";
+            this.fill.Size = new System.Drawing.Size(219, 37);
+            this.fill.TabIndex = 1;
+            this.fill.Text = "Заполнить";
+            this.fill.UseVisualStyleBackColor = true;
+            this.fill.Click += new System.EventHandler(this.fill_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(248, 379);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Результат:";
+            // 
+            // result
+            // 
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.result.Location = new System.Drawing.Point(347, 376);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(112, 26);
+            this.result.TabIndex = 3;
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.AccessibleRole = System.Windows.Forms.AccessibleRole.Table;
+            this.dataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(12, 12);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(517, 350);
+            this.dataGrid.StandardTab = true;
+            this.dataGrid.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 411);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fill);
+            this.Name = "Form1";
+            this.Text = "LR5-Matrix";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button fill;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.DataGridView dataGrid;
+    }
+}
+
