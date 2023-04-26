b9d610e42148d56b6bcd4c0e705680a7e6e5c071
Nikita76rus <56645414+Nikita76rus@users.noreply.github.com>
2023-04-26 15:36:08 +0300

    Add files via upload

---
+++ LR8/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace LR8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.path = new System.Windows.Forms.TextBox();
+            this.load = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // path
+            // 
+            this.path.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.path.Location = new System.Drawing.Point(12, 12);
+            this.path.Name = "path";
+            this.path.ReadOnly = true;
+            this.path.Size = new System.Drawing.Size(354, 26);
+            this.path.TabIndex = 0;
+            // 
+            // load
+            // 
+            this.load.BackColor = System.Drawing.Color.Tomato;
+            this.load.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.load.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.load.Location = new System.Drawing.Point(12, 44);
+            this.load.Name = "load";
+            this.load.Size = new System.Drawing.Size(354, 30);
+            this.load.TabIndex = 1;
+            this.load.Text = "Выберите файл";
+            this.load.UseVisualStyleBackColor = false;
+            this.load.Click += new System.EventHandler(this.load_Click);
+            // 
+            // start
+            // 
+            this.start.BackColor = System.Drawing.Color.Tomato;
+            this.start.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.start.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.start.Location = new System.Drawing.Point(12, 80);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(354, 30);
+            this.start.TabIndex = 2;
+            this.start.Text = "Заархивировать";
+            this.start.UseVisualStyleBackColor = false;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 130);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.load);
+            this.Controls.Add(this.path);
+            this.Name = "Form1";
+            this.Text = "LR8";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox path;
+        private System.Windows.Forms.Button load;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ LR8/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ionic.Zip;
+
+namespace LR8
+{
+    public partial class Form1 : Form
+    {
+        FolderBrowserDialog bd = new FolderBrowserDialog();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void load_Click(object sender, EventArgs e)
+        {
+            if (bd.ShowDialog() == DialogResult.OK)
+            {
+                path.Text = bd.SelectedPath;
+            }
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Zip files (*.zip)|*.zip";
+            if (path.Text != "" && sfd.ShowDialog() == DialogResult.OK)
+            {
+                ZipFile zf = new ZipFile(sfd.FileName);
+                zf.AddDirectory(bd.SelectedPath);
+                zf.Save();
+                MessageBox.Show("Архивация прошла успешно");
+             }
+        }
+    }
+}
